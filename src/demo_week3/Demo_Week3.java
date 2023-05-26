d34936587ef6e5c0c1621ab0a569227ceddc13f7
karam <karam@KARAM-PC>
2023-05-26 12:25:01 -0400

    changes

---
+++ src/demo_week3/Demo_Week3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo_week3;
+
+/**
+ *This is for deMONSTRATION FOR WEEK3 
+ * @author karam
+ */
+public class Demo_Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       System.out.println("Hello ");
+    }
+    
+}
